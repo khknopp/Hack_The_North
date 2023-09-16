0faeedfefe0a6ce4cf43daeb138f9088795272b0
saumG <saumster123@gmail.com>
2023-09-16 12:37:36 -0400

    added summarizing of highlighted section of transcript:

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from video_utils import *
 from questionGenerator import *
+from summarize import *
 from dotenv import load_dotenv
 import os
 
@@ -10,12 +11,26 @@ co = cohere.Client(COHERE_API_KEY)
 # video = "9syvZr-9xwk"
 video = "4XGGPfaTcSo"
 
-#transcript, transcript_text = run_transcript(video)
+transcript, transcript_text = run_transcript(video)
+overall_summary = " ".join(summarize_text(co, transcript_text))
 
-#m, b, e = get_fragment(transcript, 30, 55)
-#print(m,"$$$$$$$$$$$$", b, "$$$$$$$$$$$$$$$$", e)
+unfocused_fragments = []
+unfocused_fragments.append(get_fragment(transcript, 30, 55))
+unfocused_fragments.append(get_fragment(transcript, 200, 220))
+print(unfocused_fragments)
 
-check_question(co, "What is automaton M1?")
+highlighted_summary = []
+for fragment in unfocused_fragments:
+    section = fragment[1] + fragment[0] + fragment[2]
+    if len(section) >= 250:
+        highlighted_summary.append(summarize_highlighted(co, section))
+
+highlighted_summary = " ".join(highlighted_summary[0])
+
+combined_summary = combine_summaries(co, overall_summary, highlighted_summary)
+
+print(combined_summary)
+#separate based on fullstops to get bullet points
 
 #all_outputs = split_execution(co, transcript_text)
 
