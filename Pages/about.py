2c7b999b15b674565836aa29b8d0e0ff62cf3145
Allison Zhao <allisonzhao.uni@gmail.com>
2023-09-16 12:04:40 -0400

    about page content

--- Pages/about.py
+++ Pages/about.py
@@ -1,5 +1,30 @@
+from taipy.gui import Gui, Html
 about_md = """
 # <strong>About us</strong>
-We are blah2
+
+EyeTrackLearn is an innovative and intelligent eye tracking app designed to revolutionize your learning experience. With the power of advanced eye tracking technology, EyeTrackLearn ensures that you stay focused and engaged with your study materials by detecting when you look away from the screen. It then compiles important notes and provides you with quizzes to test your knowledge on the information you might have missed.
+  
+---
+
+## Key Features
+
+- **Web Design**: We create stunning websites that capture the essence of your brand.
+- **Smart Eye Tracking**: EyeTrackLearn uses cutting-edge eye tracking algorithms to monitor your gaze in real-time. It recognizes when you divert your attention from the screen, whether it's due to distractions or fatigue.
+- **Automatic Note Compilation**: When EyeTrackLearn detects that you've looked away, it takes a snapshot of the content you were studying at that moment. These snapshots are automatically compiled into concise and organized notes for later review.
+- **Personalized Quizzes**: EyeTrackLearn turns your breaks and moments of distraction into opportunities for learning. After each study session, the app generates personalized quizzes based on the notes it compiled during your study time.
+- **Progress Tracking**: Keep track of your learning progress over time. EyeTrackLearn provides insights into your attention patterns, areas of distraction, and quiz performance to help you improve your study habits.
+- **Adaptive Learning**: The app adapts to your learning style and preferences. It customizes quizzes to focus on the content you struggled with or areas where you were most distracted, ensuring efficient and targeted learning.
+
+---
+
+## Contact Us
+
+Feel free to [contact us](mailto:info@example.com) for any questions.
+
+
+
 
 """
+
+
+
