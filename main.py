77ed830d28fac951afad1589df13e3e66d2ae03c
Kajetan Knopp <kajetan@knopp.com.pl>
2023-09-16 18:44:33 +0200

    Added functions

--- main.py
+++ main.py
@@ -14,22 +14,7 @@ video = "4XGGPfaTcSo"
 transcript, transcript_text = run_transcript(video)
 overall_summary = " ".join(summarize_text(co, transcript_text))
 
-unfocused_fragments = []
-unfocused_fragments.append(get_fragment(transcript, 30, 55))
-unfocused_fragments.append(get_fragment(transcript, 200, 220))
-print(unfocused_fragments)
 
-highlighted_summary = []
-for fragment in unfocused_fragments:
-    section = fragment[1] + fragment[0] + fragment[2]
-    if len(section) >= 250:
-        highlighted_summary.append(summarize_highlighted(co, section))
-
-highlighted_summary = " ".join(highlighted_summary[0])
-
-combined_summary = combine_summaries(co, overall_summary, highlighted_summary)
-
-print(combined_summary)
 #separate based on fullstops to get bullet points
 
 open, closed, summary = split_execution(co, transcript_text)
--- summarize.py
+++ summarize.py
@@ -1,3 +1,5 @@
+from video_utils import get_fragment
+
 def summarize_text(co, text):
   transcriptionSummary = co.summarize(
     text=text,
@@ -24,3 +26,21 @@ def combine_summaries(co, overallSummary, highlightedSummary):
     )
     return combinedSummary.generations[0].text
 
+def create_fragments(transcript, boundaries):
+  fragments = []
+  for boundary in boundaries:
+    fragments.append(get_fragment(transcript, boundary[0], boundary[1]))
+  return fragments
+
+def update_summary(co, overall_summary, unfocused_fragments):
+  highlighted_summary = []
+  for fragment in unfocused_fragments:
+      section = fragment[1] + fragment[0] + fragment[2]
+      if len(section) >= 250:
+          highlighted_summary.append(summarize_highlighted(co, section))
+
+  highlighted_summary = " ".join(highlighted_summary[0])
+
+  combined_summary = combine_summaries(co, overall_summary, highlighted_summary)
+  
+  return combined_summary
