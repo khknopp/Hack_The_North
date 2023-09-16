ac51e587e7b1bb5def1ec93d12c95453a54c553d
saumG <saumster123@gmail.com>
2023-09-16 15:40:32 -0400

    added threshold for similarity comparision between overall summary and highlight summary

--- final_summary.py
+++ final_summary.py
@@ -1,22 +1,15 @@
 import spacy
 
-# Load the spaCy English model
 nlp = spacy.load("en_core_web_lg")
 
-def replace_less_detailed_sentences(overall_summary, highlighted_sections):
-    # Initialize an empty result list
-    result_summary = []
-
-    # Loop through each paraphrased subsection
-    for paraphrased_sentence in highlighted_sections:
-        # Calculate similarity scores between the paraphrased sentence and sentences in the overall summary
+def replace_less_detailed_sentences(overall_summary, highlighted_summary, similarity_threshold = 0.6):
+    for paraphrased_sentence in highlighted_summary:
         similarity_scores = [nlp(paraphrased_sentence).similarity(nlp(sentence)) for sentence in overall_summary]
 
-        # Find the index of the sentence in the overall summary with the highest similarity score
         max_similarity_index = similarity_scores.index(max(similarity_scores))
 
-        # Replace the sentence in the overall summary with the paraphrased subsection
-        overall_summary[max_similarity_index] = paraphrased_sentence
+        if max(similarity_scores) > similarity_threshold:
+            overall_summary[max_similarity_index] = paraphrased_sentence
 
     return overall_summary
 
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ load_dotenv()
 COHERE_API_KEY = os.getenv('COHERE_API_KEY')
 co = cohere.Client(COHERE_API_KEY)
 
-# video = "9syvZr-9xwk"
-video = "4XGGPfaTcSo"
+video = "9syvZr-9xwk"
+# video = "4XGGPfaTcSo"
 
 transcript, transcript_text = run_transcript(video)
 # overall_summary = " ".join(summarize_text(co, transcript_text))
@@ -19,7 +19,7 @@ transcript, transcript_text = run_transcript(video)
 open, closed, summary = split_execution(co, transcript_text)
 #get_questions(co, open, closed)
 
-fragments = create_fragments(transcript, [[50, 100], [200, 230]])
+fragments = create_fragments(transcript, [[50, 100], [200, 230], [200, 230]])
 
 print(update_summary(co, summary, fragments))
 
