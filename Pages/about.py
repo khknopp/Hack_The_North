0bcee67d039ae50c4605d0a8fe9f8b1321908375
Allison Zhao <allisonzhao.uni@gmail.com>
2023-09-16 13:19:48 -0400

    frontend UI

--- Pages/about.py
+++ Pages/about.py
@@ -4,6 +4,10 @@ about_md = """
 
 EyeTrackLearn is an innovative and intelligent eye tracking app designed to revolutionize your learning experience. With the power of advanced eye tracking technology, EyeTrackLearn ensures that you stay focused and engaged with your study materials by detecting when you look away from the screen. It then compiles important notes and provides you with quizzes to test your knowledge on the information you might have missed.
   
+<|{"image1.webp"}|image|height=250px|width=450px|on_action=image_action|>
+<|{"image2.webp"}|image|height=250px|width=250px|on_action=image_action|>
+<|{"image3.png"}|image|height=250px|width=450px|on_action=image_action|>
+
 ---
 
 ## Key Features
@@ -21,6 +25,7 @@ EyeTrackLearn is an innovative and intelligent eye tracking app designed to revo
 
 Feel free to [contact us](mailto:info@example.com) for any questions.
 
+<br/>
 
 
 
--- Pages/home.py
+++ Pages/home.py
@@ -1,7 +1,6 @@
 home_md = """
 # Welcome to eye tracking stuff
 ---
-<p><a id="myAnchor" href="https://www.w3schools.com/">W3Schools</a></p>
 
 <p>Click the button to display the value of the href attribute of the link above.</p>
 
