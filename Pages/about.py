6aeb6ce52eb85b94003f56801c620b13fbac42d5
Brian <brianfransis.moniaga@gmail.com>
2023-09-17 03:26:23 -0400

    Name change

--- Pages/about.py
+++ Pages/about.py
@@ -1,8 +1,8 @@
 from taipy.gui import Gui, Html
 about_md = """
-# <strong>About EyeTrackLearn</strong>
+# <strong>About EyeQ</strong>
 
-EyeTrackLearn is an innovative and trained eye tracking app designed to revolutionize your learning experience. Utilizing advanced eye tracking technology, EyeTrackLearn ensures that you are able to stay focused and engaged with your study materials by detecting when you look away from the screen. It then compiles notes and provides you with quiz questions to test your knowledge on any information you might have missed.
+EyeQ is an innovative and trained eye tracking app designed to revolutionize your learning experience. Utilizing advanced eye tracking technology, EyeTrackLearn ensures that you are able to stay focused and engaged with your study materials by detecting when you look away from the screen. It then compiles notes and provides you with quiz questions to test your knowledge on any information you might have missed.
 ## 
 <|{"image1.webp"}|image|height=250px|width=450px|>
 <|{"image2.webp"}|image|height=250px|width=250px|>
@@ -13,17 +13,17 @@ EyeTrackLearn is an innovative and trained eye tracking app designed to revoluti
 ## Key Features
 
 - **Web Design**: We create stunning websites that capture the essence of your brand.
-- **Smart Eye Tracking**: EyeTrackLearn uses cutting-edge eye tracking algorithms to monitor your gaze in real-time. It recognizes when you divert your attention from the screen, whether it's due to distractions or fatigue.
-- **Automatic Note Compilation**: When EyeTrackLearn detects that you've looked away, it takes a snapshot of the content you were studying at that moment. These snapshots are automatically compiled into concise and organized notes for later review.
-- **Personalized Quizzes**: EyeTrackLearn turns your breaks and moments of distraction into opportunities for learning. After each study session, the app generates personalized quizzes based on the notes it compiled during your study time.
-- **Progress Tracking**: Keep track of your learning progress over time. EyeTrackLearn provides insights into your attention patterns, areas of distraction, and quiz performance to help you improve your study habits.
+- **Smart Eye Tracking**: EyeQ uses cutting-edge eye tracking algorithms to monitor your gaze in real-time. It recognizes when you divert your attention from the screen, whether it's due to distractions or fatigue.
+- **Automatic Note Compilation**: When EyeQ detects that you've looked away, it takes a snapshot of the content you were studying at that moment. These snapshots are automatically compiled into concise and organized notes for later review.
+- **Personalized Quizzes**: EyeQ turns your breaks and moments of distraction into opportunities for learning. After each study session, the app generates personalized quizzes based on the notes it compiled during your study time.
+- **Progress Tracking**: Keep track of your learning progress over time. EyeQ provides insights into your attention patterns, areas of distraction, and quiz performance to help you improve your study habits.
 - **Adaptive Learning**: The app adapts to your learning style and preferences. It customizes quizzes to focus on the content you struggled with or areas where you were most distracted, ensuring efficient and targeted learning.
 
 ---
 
 ## Contact Us
 
-Feel free to contact us for any questions at: hello@eyetracklearn.com
+Feel free to contact us for any questions!
 
 ## 
 
