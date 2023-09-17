2360094a655f1d1f9c33db93cf9aff4da1345b67
Kajetan Knopp <kajetan@knopp.com.pl>
2023-09-17 06:41:01 +0200

    Camera better

--- tracking.py
+++ tracking.py
@@ -4,18 +4,35 @@ import cv2
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
 
-text = "Paying attention:)"
+text = ""
 
 while True:
+    # We get a new frame from the webcam
     _, frame = webcam.read()
+
+    # We send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
+
+    frame = gaze.annotated_frame()
+    text = ""
 
     new_frame = gaze.annotated_frame()
     hor = gaze.horizontal_ratio()
     ver = gaze.vertical_ratio()
+
     if(hor != None and ver != None):
         if(hor > 0.9 or ver > 0.9 or ver < 0.1 or hor < 0.1):
             text = "Not paying attention!"
+        else:
+            text = "Paying attention!"
 
-    cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    cv2.imshow(text, new_frame)
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+
+    cv2.imshow("Demo", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+   
+webcam.release()
+cv2.destroyAllWindows()
