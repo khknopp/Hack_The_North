bc4c40bf1ca92b631b26b23265d78a1c378c32d2
Allison Zhao <allisonzhao.uni@gmail.com>
2023-09-17 09:45:35 -0400

    Add screenshots to about page

--- Pages/about.py
+++ Pages/about.py
@@ -4,9 +4,9 @@ about_md = """
 
 EyeTrackLearn is an innovative and trained eye tracking app designed to revolutionize your learning experience. Utilizing advanced eye tracking technology, EyeTrackLearn ensures that you are able to stay focused and engaged with your study materials by detecting when you look away from the screen. It then compiles notes and provides you with quiz questions to test your knowledge on any information you might have missed.
 ## 
-<|{"image1.webp"}|image|height=250px|width=450px|>
-<|{"image2.webp"}|image|height=250px|width=250px|>
-<|{"image3.png"}|image|height=250px|width=450px|>
+<|{"screenshot1.png"}|image|height=250px|width=450px|>
+<|{"screenshot2.png"}|image|height=250px|width=250px|>
+<|{"screenshot3.png"}|image|height=250px|width=450px|>
 
 ---
 
@@ -18,12 +18,6 @@ EyeTrackLearn is an innovative and trained eye tracking app designed to revoluti
 - **Personalized Quizzes**: EyeTrackLearn turns your breaks and moments of distraction into opportunities for learning. After each study session, the app generates personalized quizzes based on the notes it compiled during your study time.
 - **Progress Tracking**: Keep track of your learning progress over time. EyeTrackLearn provides insights into your attention patterns, areas of distraction, and quiz performance to help you improve your study habits.
 - **Adaptive Learning**: The app adapts to your learning style and preferences. It customizes quizzes to focus on the content you struggled with or areas where you were most distracted, ensuring efficient and targeted learning.
-
----
-
-## Contact Us
-
-Feel free to contact us for any questions at: hello@eyetracklearn.com
 
 ## 
 
